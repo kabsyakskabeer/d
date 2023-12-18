54373fb49138fdf6b5127fb3ff565755812baca1
kabsyaks kabeer <36848823+kabsyakskabeer@users.noreply.github.com>
2023-12-18 11:02:09 +0100

    hbg

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,12 @@
 self.addEventListener("fetch", function(event) {
     console.log("Fetch request for:", event.request.url);
     });
+
+    self.addEventListener("install", function(event) {
+        event.waitUntil(
+        caches.open("gih-cache").then(function(cache) {
+       // return cache.add("/index-offline.html");
+            console.log('installing');
+        })
+        );
+        });
